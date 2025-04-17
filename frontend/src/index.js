02d1a95ece9c015e7fe281b57941cda3e23f99a6
byessilyurt <y.yesilyurt14@gmail.com>
2025-04-17 20:44:10 +0000

    Add styles for login, user profile, and authentication-related components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Auth0ProviderWithHistory from './auth/auth0-provider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
